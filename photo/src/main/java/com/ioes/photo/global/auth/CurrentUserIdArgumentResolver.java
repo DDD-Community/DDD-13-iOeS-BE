ad333efa057a368f07436c08529c73e64e3633a8
hwangjeyeon <kingpele9018@gmail.com>
2026-05-13 22:52:24 +0900

    :wrench: config: 현재 로그인한 사용자 ID를 파라미터로 주입하는 애노테이션 및 설정 개발 #38
    Signed-off-by: hwangjeyeon <kingpele9018@gmail.com>

---
+++ photo/src/main/java/com/ioes/photo/global/auth/CurrentUserIdArgumentResolver.java
@@ -0,0 +1,40 @@
+package com.ioes.photo.global.auth;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * CurrentUserId 어노테이션이 붙은 Long 파라미터에 현재 사용자 ID를 주입하는 리졸버.
+ *
+ * @author 황제연
+ */
+@Component
+public class CurrentUserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUserId.class)
+            && Long.class.equals(parameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return null;
+        }
+        try {
+            return Long.parseLong(authentication.getName());
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+}
