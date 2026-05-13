876d9659be39a375e279f6accb5ba108e004727d
hwangjeyeon <kingpele9018@gmail.com>
2026-05-13 22:35:27 +0900

    :memo: chore: 미사용 쿼리 삭제 및 주석추가 #38
    Signed-off-by: hwangjeyeon <kingpele9018@gmail.com>

--- photo/src/main/java/com/ioes/photo/domain/spot/repository/SpotRepository.java
+++ photo/src/main/java/com/ioes/photo/domain/spot/repository/SpotRepository.java
@@ -15,8 +15,6 @@ import org.springframework.data.repository.query.Param;
  *
  * 외부 API 스케줄러의 수집 대상 조회를 위한 메서드들을 제공한다.
  *
- * findAllInViewport 개발 > 황제연
- *
  * @author 김성민, 황제연
  */
 public interface SpotRepository extends JpaRepository<Spot, Long> {
@@ -40,15 +38,4 @@ public interface SpotRepository extends JpaRepository<Spot, Long> {
 
     @Query(value = "SELECT * FROM spots WHERE id = :spotId", nativeQuery = true)
     Optional<Spot> findByIdIncludingDeleted(@Param("spotId") Long spotId);
-
-    @Query(
-        value = "SELECT s.* FROM spots s " +
-                "WHERE s.location && ST_MakeEnvelope(:minLng, :minLat, :maxLng, :maxLat, 4326) " +
-                "AND s.status = :status " +
-                "AND s.deleted_at IS NULL",
-        nativeQuery = true
-    )
-    List<Spot> findAllInViewport(@Param("minLat") double minLat, @Param("maxLat") double maxLat,
-                                  @Param("minLng") double minLng, @Param("maxLng") double maxLng,
-                                  @Param("status") String status);
 }
