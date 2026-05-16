fab14c998e6c868eae0f17993e23b78533a2cff1
hwangjeyeon <kingpele9018@gmail.com>
2026-05-17 01:26:48 +0900

    :sparkles: feat: 로그아웃 AccessToken 블랙리스트 등록 #45
    Signed-off-by: hwangjeyeon <kingpele9018@gmail.com>

--- photo/src/main/java/com/ioes/photo/global/config/security/properties/TokenProperties.java
+++ photo/src/main/java/com/ioes/photo/global/config/security/properties/TokenProperties.java
@@ -5,10 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 /**
  * 토큰 관련 설정 프로퍼티.
  *
- * @param refreshExpirationMs Refresh Token 유효 시간
- * @param refreshKeyPrefix Redis Refresh Token 키 prefix
- * @param userTokensKeyPrefix Redis 유저별 토큰 Set 키 prefix
- * @param providerRtKeyPrefix Redis Provider Refresh Token 키 prefix
+ * @param refreshExpirationMs       Refresh Token 유효 시간
+ * @param refreshKeyPrefix          Redis Refresh Token 키 prefix
+ * @param userTokensKeyPrefix       Redis 유저별 토큰 Set 키 prefix
+ * @param providerRtKeyPrefix       Redis Provider Refresh Token 키 prefix
+ * @param blacklistKeyPrefix        Redis Access Token 블랙리스트 키 prefix
+ * @param restoreTokenExpirationMs  복구 토큰 유효 시간 (5분)
  * @author 황제연
  */
 @ConfigurationProperties(prefix = "app.token")
@@ -16,7 +18,8 @@ public record TokenProperties(
     long refreshExpirationMs,
     String refreshKeyPrefix,
     String userTokensKeyPrefix,
-    String providerRtKeyPrefix
+    String providerRtKeyPrefix,
+    String blacklistKeyPrefix,
     long restoreTokenExpirationMs
 ) {
 }
